2c7fe67b56906a2fb218a9369e3ece9fac6b2746
carlceca05 <juan.cebalos@cecaes.edu.mx>
2022-06-08 13:32:53 -0500

    08/06/22 13:32

--- js/respuestas.js
+++ js/respuestas.js
@@ -1,5 +1,5 @@
 function resultado() {
-    var p1, p2, p3, p4, p5, p6, p7, p8, p9, /*p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31,*/ nota;
+    var p1, p2, p3,/* p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31,*/ nota;
 
     // 1a pregunta
     if (document.getElementById('p1-2').checked==true) {p1=1}
@@ -10,6 +10,7 @@ function resultado() {
     // 3a pregunta
     if (document.getElementById('p3-4').checked==true) {p3=1}
     else {p3=0}
+    /*
     // 4a pregunta
     if (document.getElementById('p4-4').checked==true) {p4=1}
     else {p4=0}
@@ -96,7 +97,7 @@ function resultado() {
     if (document.getElementById('p29-1').checked==true) {p10=1}
     else {p31=0}*/
 
-    nota=p1+p2+p3+p4+p5/*+p6+p7+p8+p9+p10+p11+p12+p13+p14+p15+p16+p17+p18+p19+p20+p21+p22+p23+p24+p25+p26+p27+p28+p29+p30+p31*/;
+    nota=p1+p2+p3/*+p4+p5+p6+p7+p8+p9+p10+p11+p12+p13+p14+p15+p16+p17+p18+p19+p20+p21+p22+p23+p24+p25+p26+p27+p28+p29+p30+p31*/;
     //alert(" Aciertos: " + nota);
     document.getElementById("aqui").innerHTML =   'Aciertos: ' + nota + '<br> Revisa tus respuestas';
       
@@ -111,9 +112,9 @@ function resultado() {
     document.getElementById("Correct01").classList.toggle("ans-correct");
     document.getElementById("Correct02").classList.toggle("ans-correct");
     document.getElementById("Correct03").classList.toggle("ans-correct");
-    document.getElementById("Correct04").classList.toggle("ans-correct");
+    /*document.getElementById("Correct04").classList.toggle("ans-correct");
     document.getElementById("Correct05").classList.toggle("ans-correct");
-    /*document.getElementById("Correct06").classList.toggle("ans-correct");
+    document.getElementById("Correct06").classList.toggle("ans-correct");
     document.getElementById("Correct07").classList.toggle("ans-correct");
     document.getElementById("Correct08").classList.toggle("ans-correct");
     document.getElementById("Correct09").classList.toggle("ans-correct");
