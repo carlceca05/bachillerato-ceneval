4e4fb0bc6414952cbd9f5a5086977033c8be1fcd
carlceca05 <juan.cebalos@cecaes.edu.mx>
2022-06-30 12:46:02 -0500

    30/06/22 12:45

--- js/respuestas-matematicas.js
+++ js/respuestas-matematicas.js
@@ -44,7 +44,7 @@ function resultado() {
     if (document.getElementById('p14-A').checked==true) {p14=1}
     else {p14=0}
     // 10a pregunta
-    if (document.getElementById('p15-1').checked==true) {p15=1}
+    if (document.getElementById('p15-A').checked==true) {p15=1}
     else {p15=0}
     // 10a pregunta
     if (document.getElementById('p16-C').checked==true) {p16=1}
@@ -94,11 +94,112 @@ function resultado() {
     //pregunta 31
     if (document.getElementById('p31-D').checked==true) {p31=1}
     else {p31=0}
+    //pregunta 31
+    if (document.getElementById('p32-D').checked==true) {p32=1}
+    else {p32=0}
+    //pregunta 31
+    if (document.getElementById('p33-B').checked==true) {p33=1}
+    else {p33=0}
+    //pregunta 31
+    if (document.getElementById('p34-D').checked==true) {p34=1}
+    else {p34=0}
+    //pregunta 31
+    if (document.getElementById('p35-B').checked==true) {p35=1}
+    else {p35=0}
+    //pregunta 31
+    if (document.getElementById('p36-D').checked==true) {p36=1}
+    else {p36=0}
+    //pregunta 31
+    if (document.getElementById('p37-B').checked==true) {p37=1}
+    else {p37=0}
+    //pregunta 31
+    if (document.getElementById('p38-A').checked==true) {p38=1}
+    else {p38=0}
+    //pregunta 31
+    if (document.getElementById('p39-C').checked==true) {p39=1}
+    else {p39=0}
+    //pregunta 31
+    if (document.getElementById('p40-D').checked==true) {p40=1}
+    else {p40=0}
+    //pregunta 31
+    if (document.getElementById('p41-C').checked==true) {p41=1}
+    else {p41=0}
+    //pregunta 31
+    if (document.getElementById('p42-A').checked==true) {p42=1}
+    else {p42=0}
+    //pregunta 31
+    if (document.getElementById('p43-B').checked==true) {p43=1}
+    else {p43=0}
+    //pregunta 31
+    if (document.getElementById('p44-A').checked==true) {p44=1}
+    else {p44=0}
+    //pregunta 31
+    if (document.getElementById('p45-D').checked==true) {p45=1}
+    else {p45=0}
+    //pregunta 31
+    if (document.getElementById('p46-A').checked==true) {p46=1}
+    else {p46=0}
+    //pregunta 31
+    if (document.getElementById('p47-C').checked==true) {p47=1}
+    else {p47=0}
+    //pregunta 31
+    if (document.getElementById('p48-B').checked==true) {p48=1}
+    else {p48=0}
+    //pregunta 49
+    if (document.getElementById('p49-C').checked==true) {p49=1}
+    else {p49=0}
+    //pregunta 49
+    if (document.getElementById('p50-A').checked==true) {p50=1}
+    else {p50=0}
+    //pregunta 49
+    if (document.getElementById('p51-B').checked==true) {p51=1}
+    else {p51=0}
+    //pregunta 49
+    if (document.getElementById('p52-D').checked==true) {p52=1}
+    else {p52=0}
+    //pregunta 49
+    if (document.getElementById('p53-C').checked==true) {p53=1}
+    else {p53=0}
+    //pregunta 49
+    if (document.getElementById('p54-C').checked==true) {p54=1}
+    else {p54=0}
+    //pregunta 49
+    if (document.getElementById('p55-D').checked==true) {p55=1}
+    else {p55=0}
+    //pregunta 49
+    if (document.getElementById('p56-D').checked==true) {p56=1}
+    else {p56=0}
+    //pregunta 49
+    if (document.getElementById('p57-B').checked==true) {p57=1}
+    else {p57=0}
+    //pregunta 49
+    if (document.getElementById('p58-A').checked==true) {p58=1}
+    else {p58=0}
+    //pregunta 49
+    if (document.getElementById('p59-C').checked==true) {p59=1}
+    else {p59=0}
+    //pregunta 49
+    if (document.getElementById('p60-B').checked==true) {p60=1}
+    else {p60=0}
+    //pregunta 49
+    if (document.getElementById('p61-A').checked==true) {p61=1}
+    else {p61=0}
+    //pregunta 49
+    if (document.getElementById('p62-D').checked==true) {p62=1}
+    else {p62=0}
+    //pregunta 49
+    if (document.getElementById('p63-D').checked==true) {p63=1}
+    else {p63=0}
+    //pregunta 49
+    if (document.getElementById('p64-D').checked==true) {p64=1}
+    else {p64=0}
 
-    nota=p1+p2+p3+p4+p5+p6+p7+p8+p9+p10+p11+p12+p13+p14+p15+p16+p17+p18+p19+p20+p21+p22+p23+p24+p25+p26+p27+p28+p29+p30+p31;  //64
+    nota=p1+p2+p3+p4+p5+p6+p7+p8+p9+p10+p11+p12+p13+p14+p15+p16+p17+p18+p19+p20+p21+p22+p23+p24+p25+p26+p27+p28+p29+p30+p31+p32+p33+p34+p35+p36+p37+p38+p39+p40+p41+p42+p43+p44+p45+p46+p47+p48+p49+p50+p51+p52+p53+p54+p55+p56+p57+p58+p60+p61+p62+p63+p64;  //64
+    
     //alert(" Aciertos: " + nota);
+    
     document.getElementById("aqui").innerHTML =   'Aciertos: ' + nota + '<br> Revisa tus respuestas';
-      
+    
     //document.write( "Aciertos:" + nota);
     
     //window.location = 'cuestionario.html'
@@ -137,5 +238,38 @@ function resultado() {
     document.getElementById("Correct28").classList.toggle("ans-correct");
     document.getElementById("Correct29").classList.toggle("ans-correct");
     document.getElementById("Correct30").classList.toggle("ans-correct");
-    document.getElementById("Correct31").classList.toggle("ans-correct"); 
+    document.getElementById("Correct31").classList.toggle("ans-correct");
+    document.getElementById("Correct32").classList.toggle("ans-correct");
+    document.getElementById("Correct33").classList.toggle("ans-correct");
+    document.getElementById("Correct34").classList.toggle("ans-correct");
+    document.getElementById("Correct35").classList.toggle("ans-correct");
+    document.getElementById("Correct36").classList.toggle("ans-correct");
+    document.getElementById("Correct37").classList.toggle("ans-correct");
+    document.getElementById("Correct38").classList.toggle("ans-correct");
+    document.getElementById("Correct39").classList.toggle("ans-correct");
+    document.getElementById("Correct40").classList.toggle("ans-correct");
+    document.getElementById("Correct41").classList.toggle("ans-correct");
+    document.getElementById("Correct42").classList.toggle("ans-correct");
+    document.getElementById("Correct43").classList.toggle("ans-correct");
+    document.getElementById("Correct44").classList.toggle("ans-correct");
+    document.getElementById("Correct45").classList.toggle("ans-correct");
+    document.getElementById("Correct46").classList.toggle("ans-correct");
+    document.getElementById("Correct47").classList.toggle("ans-correct");
+    document.getElementById("Correct48").classList.toggle("ans-correct");
+    document.getElementById("Correct49").classList.toggle("ans-correct");
+    document.getElementById("Correct50").classList.toggle("ans-correct");
+    document.getElementById("Correct51").classList.toggle("ans-correct"); 
+    document.getElementById("Correct52").classList.toggle("ans-correct");
+    document.getElementById("Correct53").classList.toggle("ans-correct");
+    document.getElementById("Correct54").classList.toggle("ans-correct");
+    document.getElementById("Correct55").classList.toggle("ans-correct");
+    document.getElementById("Correct56").classList.toggle("ans-correct");
+    document.getElementById("Correct57").classList.toggle("ans-correct");
+    document.getElementById("Correct58").classList.toggle("ans-correct");
+    document.getElementById("Correct59").classList.toggle("ans-correct");
+    document.getElementById("Correct60").classList.toggle("ans-correct");
+    document.getElementById("Correct61").classList.toggle("ans-correct");
+    document.getElementById("Correct62").classList.toggle("ans-correct");
+    document.getElementById("Correct63").classList.toggle("ans-correct");
+    document.getElementById("Correct64").classList.toggle("ans-correct");
 }
